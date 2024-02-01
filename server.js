d2baab6d154e00edef7867fd824ff2015d24737d
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-01-31 19:30:51 -0600

    created schema

---
+++ server.js
@@ -0,0 +1,9 @@
+
+// open the application and be prompted. ( using inquirer)
+// view all depts, view all roles, view all employeees, add dept, add role, add employee,update employee role
+// when view dept is chosen a table is shown with dept names and dept ids. ill do this by creating a schema and then doing  a get request in order to view the information.
+// when view all roles is chosen a table with job title, role id, the dept it belongs to, and the salary for the role. this will be done once again by creating a table with schema and then using a GET request to view the info
+// view all employees a table is shown with employee id, first and last, job title, dept, salaries, and managers the empoyee reports to. Also using a GET request
+// add a dept - post request to add to dept table
+// add an employee - post request to add to employee table
+// update employee role get back selected employee modify info and then send it back to the table. USING GET/POST
